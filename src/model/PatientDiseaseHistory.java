0818515e7ecfa89b05b21026a2ac8afb25045a3d
Soloplay31@ <polatozdemirs@gmail.com>
2025-03-03 22:42:41 +0200

    PatientDiseaseHistory , setters,variables added

--- src/model/PatientDiseaseHistory.java
+++ src/model/PatientDiseaseHistory.java
@@ -9,11 +9,47 @@ public class PatientDiseaseHistory {
 	private LocalDate diseaseStartingDate;
 	private boolean	isPresent;
 	private Disease disease;
-	private DoctorType Doctor;
+	private Doctor treatingDoctor;
 	private String notes;
 	private int severity;
 	private ArrayList<Medicine> prescribedMedicines = new ArrayList<Medicine>();
 	private static long counter = 0;
+	public long gethID() {
+		return hID;
+		
+	//1.setters	
+	}
+	public LocalDate getDiseaseStartingDate() {
+		return diseaseStartingDate;
+	}
+	public boolean isPresent() {
+		return isPresent;
+	}
+	public Disease getDisease() {
+		return disease;
+	}
+	public Doctor getTreatingDoctor() {
+		return treatingDoctor;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public int getSeverity() {
+		return severity;
+	}
+	public ArrayList<Medicine> getPrescribedMedicines() {
+		return prescribedMedicines;
+	}
+	
+	//2.getters
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
